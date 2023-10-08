b8437e0164fa3b38ab34cdcb4b56d4655a508567
semi <semilin@disroot.org>
2023-10-07 22:06:53 -0700

    fix lsb, add lsb-distance, add same-row rolls

--- metrics/base.py
+++ metrics/base.py
@@ -11,9 +11,15 @@ def direction(a, b):
         return Direction.INWARD
     return Direction.OUTWARD
 
+def x_distance(a, b):
+    return abs(a.x - b.x)
+
+def y_distance(a, b):
+    return abs(a.y - b.y)
+
 def distance(a, b):
-    return sqrt((a.x - b.x) ** 2 +
-         (a.y - b.y) ** 2)
+    return sqrt(x_distance(a, b) ** 2 +
+         y_distance(a, b) ** 2)
 
 def is_sfr(a, b):
     return a == b
@@ -21,6 +27,11 @@ def is_sfr(a, b):
 def is_sfb(a, b):
     return (not is_sfr(a, b)
             and a.finger == b.finger)
+
+def is_lsb(a, b):
+    return (a.finger.hand() == b.finger.hand()
+            and abs(a.finger.kind().value - b.finger.kind().value) == 1
+            and x_distance(a, b) >= 2)
 
 def is_alternate(a, b, c):
     return (a.finger.hand() != b.finger.hand()
@@ -37,13 +48,20 @@ def is_redirect(a, b, c):
             and b.finger != c.finger
             and direction(a, b) != direction(b, c))
 
+def is_same_row_roll(a, b, c):
+    return (is_roll(a, b, c)
+            and a.pos.row == b.pos.row == c.pos.row)
+
 METRIC_LIST = [
     Metric("Same-finger Repeats", "sfr", NgramType.BIGRAM, is_sfr, 1),
     Metric("Same-finger Bigrams", "sfb", NgramType.BIGRAM, is_sfb, 1),
     Metric("Same-finger Skipgrams", "sfs", NgramType.SKIPGRAM, is_sfb, 1),
     Metric("Same-finger Bigram Distance", "sfb-dist", NgramType.BIGRAM, is_sfb, distance),
     Metric("Same-finger Skipgram Distance", "sfs-dist", NgramType.SKIPGRAM, is_sfb, distance),
+    Metric("Lateral Stretch Bigrams", "lsb", NgramType.BIGRAM, is_lsb, 1),
+    Metric("Lateral Stretch Bigram Distance", "lsb-dist", NgramType.BIGRAM, is_lsb, x_distance),
     Metric("Alternates", "alt", NgramType.TRIGRAM, is_alternate, 1),
     Metric("Rolls", "roll", NgramType.TRIGRAM, is_roll, 1),
+    Metric("Same-row Rolls", "sr-roll", NgramType.TRIGRAM, is_same_row_roll, 1),
     Metric("Redirects", "redir", NgramType.TRIGRAM, is_redirect, 1)
 ]
