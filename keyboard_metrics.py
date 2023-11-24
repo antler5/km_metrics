d2246b6418e1cdb181f3a33855c8703896b37eef
semi <semilin@disroot.org>
2023-11-24 11:33:00 -0800

    add keyboard_metrics.py

---
+++ keyboard_metrics.py
@@ -0,0 +1,18 @@
+## Import whichever keyboard and metric modules you want here.
+from keyboards import matrix, columnar, ansi, ansi_angle
+from metrics import base
+
+## The second field for each keyboard is the set of metric lists. You
+## can have multiple per keyboard.
+KEYBOARDS = [
+    (matrix, [base]),
+    (columnar, [base]),
+    (ansi, [base]),
+    (ansi_angle, [base])
+]
+
+
+def flat_metric_list(arr):
+    return [metric for mlist in arr for metric in mlist.METRIC_LIST]
+
+KEYBOARDS = [(kb.KEYBOARD, flat_metric_list(mlist)) for (kb, mlist) in KEYBOARDS]
