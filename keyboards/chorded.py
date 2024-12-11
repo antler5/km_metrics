862ae74c619a07b6694f7fb3752d0df4a58c9fc2
antlers <antlers@illucid.net>
2024-12-11 14:50:52 -0800

    keyboards/chorded.py: Add full-row combos, and remove vertical triples
    * keyboards/chorded.py: Add full-row combos, and remove vertical triples

--- keyboards/chorded.py
+++ keyboards/chorded.py
@@ -34,10 +34,18 @@ combos = combos.union(set([
     for b in coords
     for c in coords
     if len(set([a,b,c])) == 3
+    # Stacks on the same finger are ok, but not with a third key.
+    and len(set(map(lambda x: x[0], [a,b,c]))) == 3
 ]))
 
 KEYBOARD.combos = [Combo(list(map(lambda col,row: coord(col,row), *zip(*xs))))
                    for xs in sorted(combos)]
+
+# Add z/space 4-chords to model artsey more accurately.
+KEYBOARD.combos += [
+    Combo([coord(0,0),coord(1,0),coord(2,0),coord(3,0)]),
+    Combo([coord(0,1),coord(1,1),coord(2,1),coord(3,1)])
+]
 
 def finger(c):
     match c:
