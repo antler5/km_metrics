64936776ba7a94d34fb6d1cb9a1e7e2feeb0e223
antlers <antlers@illucid.net>
2024-12-11 15:01:26 -0800

    keyboards/chorded.py: Remove full-row chords
    I don't want these generated, and unlike thumb-keys (which I pin in the
    generator), I don't really want these analyzed either.
    
    * keyboards/chorded.py: Remove full-row chords

--- keyboards/chorded.py
+++ keyboards/chorded.py
@@ -41,12 +41,6 @@ combos = combos.union(set([
 KEYBOARD.combos = [Combo(list(map(lambda col,row: coord(col,row), *zip(*xs))))
                    for xs in sorted(combos)]
 
-# Add z/space 4-chords to model artsey more accurately.
-KEYBOARD.combos += [
-    Combo([coord(0,0),coord(1,0),coord(2,0),coord(3,0)]),
-    Combo([coord(0,1),coord(1,1),coord(2,1),coord(3,1)])
-]
-
 def finger(c):
     match c:
         case (0,0):
